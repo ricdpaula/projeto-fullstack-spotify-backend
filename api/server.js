3bbc6ed1a3e8bad0615a0ff2fd892bc25435f87a
ricpaula <cadodepaulasilva159@gmail.com>
2025-02-26 20:42:01 -0300

    Update Server.js

--- api/server.js
+++ api/server.js
@@ -1,31 +1,33 @@
-import express from 'express';
-import { db } from './connect.js';
-import cors from 'cors';
-import dotenv from 'dotenv'
+import express from "express";
+import { db } from "./connect.js";
+import cors from "cors";
+import dotenv from "dotenv";
 
-dotenv.config({path: "../.env"})
+dotenv.config({ path: "../.env" });
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors())
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-app.get("/", (req, res)=>{
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header("Access-Control-Allow-Origin", 'true')
-    res.send('Olá, Mundo')
-})
-app.get("/artists", async (req, res)=>{
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header("Access-Control-Allow-Origin", 'true')
-    res.send(await db.collection("artists").find({}).toArray())
-})
-app.get("/songs", async (req, res)=>{
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header("Access-Control-Allow-Origin", 'true')
-    res.send(await db.collection("songs").find({}).toArray())
-})
+app.get("/", (req, res) => {
+  res.send("Olá, Mundo");
+});
+app.get("/artists", async (req, res) => {
+  res.send(await db.collection("artists").find({}).toArray());
+});
+app.get("/songs", async (req, res) => {
+  res.send(await db.collection("songs").find({}).toArray());
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Servidor está escutando na porta: ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Servidor está escutando na porta: ${PORT}`);
+});
