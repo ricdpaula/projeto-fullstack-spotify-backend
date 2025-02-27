2452514b9d22f449140159ddb07ee4a6785523f8
ricpaula <cadodepaulasilva159@gmail.com>
2025-02-27 14:26:27 -0300

    Update server

--- api/server.js
+++ api/server.js
@@ -10,22 +10,16 @@ const PORT = process.env.PORT;
 
 app.use(cors());
 
-app.use(cors(), function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://projeto-fullstack-spotify.vercel.app/"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 app.get("/", (req, res) => {
+  res.header({ "Access-Control-Allow-Origin": "http://localhost:3000" });
   res.send("Olá, Mundo");
 });
 app.get("/artists", async (req, res) => {
+  res.header({ "Access-Control-Allow-Origin": "http://localhost:3000" })
   res.send(await db.collection("artists").find({}).toArray());
 });
 app.get("/songs", async (req, res) => {
+  res.header({ "Access-Control-Allow-Origin": "http://localhost:3000" })
   res.send(await db.collection("songs").find({}).toArray());
 });
 
