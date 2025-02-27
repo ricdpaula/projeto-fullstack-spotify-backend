a5e6dd35018143892d3aa92cbf3e7d6446ac6841
ricpaula <cadodepaulasilva159@gmail.com>
2025-02-27 14:22:36 -0300

    Update server

--- api/server.js
+++ api/server.js
@@ -10,6 +10,15 @@ const PORT = process.env.PORT;
 
 app.use(cors());
 
+app.use(cors(), function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://projeto-fullstack-spotify.vercel.app/"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Olá, Mundo");
 });
