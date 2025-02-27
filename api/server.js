057c3d1e5f1c5d4f81bffe84ade641fa02d7cc82
ricpaula <cadodepaulasilva159@gmail.com>
2025-02-27 14:00:14 -0300

    Update server

--- api/server.js
+++ api/server.js
@@ -8,15 +8,16 @@ dotenv.config({ path: "../.env" });
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://projeto-fullstack-spotify.vercel.app");
+app.use(cors(), function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Olá, Mundo");
